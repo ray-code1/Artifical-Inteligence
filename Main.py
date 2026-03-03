d960d20d4c47be0116a3b4936274eacf8be038a4
ray-code1 <yusong1229@gmail.com>
2026-03-03 18:00:04 +0900

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,16 @@
+print('Welcome to AI chat bot.What is your name?')
+name=input()
+print('Nice to meet you',name)
+print('How are you feeling today.Good or bad?')
+mood=input()
+if mood=='bad':
+  print('I hope you will feel better soon')
+
+elif mood=='good':
+  print('That is great')
+
+else:
+  print('401 not found')
+
+
+print('It was fun talking to you. I hope we will see each other soon.')
