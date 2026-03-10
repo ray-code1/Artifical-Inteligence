d222dc8aca0390c6c7d9b0496ffbbdf11b2ab05f
ray-code1 <yusong1229@gmail.com>
2026-03-10 18:10:43 +0900

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,24 @@
+print('hi my name is Ethan,what is your name?')
+name = input()
+
+print('what a good name to have',name)
+print(name,'are you having a good time')
+mood = input().lower()
+
+if mood=='good':
+  print('that is a very good thing to hear')
+
+elif mood=='bad':
+    print('that is very sad to hear.I hope you will feel better next time we chat')
+
+else:
+   print('i am sorry',name,'i am not abale to compute')
+
+
+
+print('it was a lovely time talking to you')
+
+
+
+
+
